d515650d33866c9683b311669b1a30c4fc2f1d91
周超 <zhouchao@zhixueyun.com>
2016-09-21 14:24:29 +0800

    websocket init

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-var express = require('express')
-var qs = require('querystring')
-var http = require('http')
-var url = require('url')
-var bodyParser = require('body-parser')
-var smslist = require('./smslist')
 var WebSocketServer  = require('ws').Server
 var wss = new WebSocketServer({ port: 9001 });
 var wsClients = {}
@@ -142,25 +136,3 @@ function sendMessageAll (type, data, clients) {
     })
   }, 1)
 }
-
-
-var app = express()
-
-app.use(bodyParser.json())
-app.all('*', (request, response, next) => {
-  console.log('requestpath:', request.path)
-  console.log('requestbody:', request.body)
-  next()
-})
-
-app.post('/github/webhook', function (req, res) {
-  var body = req.body
-  console.log(body)
-  res.end()
-})
-
-var server = app.listen(9000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('app listening at http://%s:%s', host, port);
-});
